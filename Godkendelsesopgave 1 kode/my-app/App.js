4a60b9cf874451f0ba0f3b63f8f7f6009a3fe0d0
linneaovervad <131151466+linneaovervad@users.noreply.github.com>
2024-09-29 20:16:04 +0200

    Kode til React Native App

---
+++ Godkendelsesopgave 1 kode/my-app/App.js
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; //  Importer createBottomTabNavigator fra @react-navigation/bottom-tabs
+import Icon from 'react-native-vector-icons/Ionicons'; // Importer Ionicons fra react-native-vector-icons
+import { useEffect, useState } from "react";
+import { getApps, initializeApp } from "firebase/app"; // Importer initializeApp fra "firebase/app"
+import { getDatabase } from "firebase/database";  // Importer getDatabase fra "firebase/database"
+
+import Home from './components/home'; 
+import ChoreList from './components/choreList';
+import People from './components/people';
+
+// Firebase konfiguration
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  const [databaseInstance, setDatabaseInstance] = useState(null);
+
+  useEffect(() => {
+    // Initialiser kun Firebase, hvis det ikke allerede er gjort
+    if (getApps().length === 0) {
+      const app = initializeApp(firebaseConfig);
+      console.log("Firebase initialized!");
+    }
+
+    // Initialiser databasen
+    const db = getDatabase();
+    setDatabaseInstance(db); // Gem databaseinstansen i state
+  }, []);
+
+  // Returner navigationen
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+// Tilføj ikoner til fanerne
+            if (route.name === 'Home') {
+              iconName = 'home-outline';
+            } else if (route.name === 'Chore List') {
+              iconName = 'list-outline';
+            } else if (route.name === 'People') {
+              iconName = 'people-outline';
+            }
+
+            // Returner ikon
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'lightpink',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            display: 'flex',
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        
+        {/* Videregiv databaseInstance som en prop til ChoreList */}
+        <Tab.Screen name="Chore List">
+          {props => <ChoreList {...props} database={databaseInstance} />}
+        </Tab.Screen>
+
+        <Tab.Screen name="People" component={People} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Godkendelsesopgave 1 kode/my-app/readme.md
@@ -0,0 +1,21 @@
+
+//sørg for at indsætte egen firebaseConfig i App.js
+
+//Sørg for at nagivere korrekt
+cd my-app
+
+//Start med at kør 
+npm install 
+
+//kør
+npm install firebase
+
+//kør
+npm install react-native-image-picker
+
+//kør
+npm install @react-native-community/datetimepicker
+
+//Kør 
+npx expo start --tunnel
+
